950087ba442bc863bf0a964625b28d473cc58a2d
Zachary Masaitis <legomaster119@gmail.com>
2026-09-17 21:22:05 -0400

    Utilize YIN algorithm implementation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,55 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import AudioProcessor from '../pitch-processing/audio-processor';
+import yin from '@/pitch-processing/audiojs-yin';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const DECIMAL_PLACES = 2
+
 
 export default function App() {
   const [freq, setFreq] = useState<number>()
 
-  const processor = new AudioProcessor()
-  processor.onNoteDetected = (frequency: number) => {
-    setFreq(Math.round(frequency))
-  }
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({audio: true})
+      .then(localMediaStream => {
+        let audioContext = new AudioContext()
+        let source = audioContext.createMediaStreamSource(localMediaStream)
+        let analyser = audioContext.createAnalyser()
+  
+        source.connect(analyser)
+        analyser.connect(audioContext.destination)
+        analyser.fftSize = 2048
+    
+        let bufferLength = analyser.frequencyBinCount
+        // let frequencyData = new Uint8Array(bufferLength)
+        let timeFreqData = new Float32Array(bufferLength)
+        let timeDomainData = new Float32Array(bufferLength)
+  
+        setInterval(() => {
+          // analyser.getByteFrequencyData(frequencyData)
+          analyser.getFloatFrequencyData(timeFreqData)
+          analyser.getFloatTimeDomainData(timeDomainData)
 
-  function toggleMic() {
-    if (!processor.isRunning) { processor.start() }
-    else { processor.stop() }
-  }
+          const freq = yin(timeDomainData, { fs: audioContext.sampleRate })?.freq
+          
+          setFreq(
+            Math.round((freq ?? 0) * (10 ** DECIMAL_PLACES)) / (10 ** DECIMAL_PLACES)  
+          )
+        }, 50)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Button title="Start" onPress={() => toggleMic()}/>
       <View>
-        <Text>
-          {freq} hz
-        </Text>
+        <View style={styles.text}>
+          <Text>{freq}</Text>
+          <Text>hz</Text>
+        </View>
       </View>
     </View>
   );
@@ -36,4 +64,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
     padding: 10,
   },
+  text: {
+    width: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
 });
