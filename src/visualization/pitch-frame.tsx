aad4eabd67f1a23d65100e5f2f9d2c390da755e9
Zachary Masaitis <legomaster119@gmail.com>
2026-09-24 22:16:54 -0400

    Add reference lines for As

--- src/visualization/pitch-frame.tsx
+++ src/visualization/pitch-frame.tsx
@@ -6,23 +6,33 @@ import { PitchProps } from "./types";
 const VBOX_W = 500;
 const VBOX_H = 270;
 
+const MIN_FREQ = 90;
+const MAX_FREQ = 2000;
+
 const READING_INCREMENT = 1;
 
 export function PitchFrame(props: PitchProps) {
   const REFERENCE_FREQ = intonation.A;
-  const CENTS_MIN = intonation.intervalToCents(REFERENCE_FREQ, 10)
-  const CENTS_MAX = intonation.intervalToCents(REFERENCE_FREQ, 15000)
+  const CENTS_MIN = intonation.intervalToCents(REFERENCE_FREQ, MIN_FREQ)
+  const CENTS_MAX = intonation.intervalToCents(REFERENCE_FREQ, MAX_FREQ)
   
   const [scrollBarX, setScrollBarX] = useState<number>(0)
   const [yValues, setYValues] = useState<number[]>([])
   const [path, setPath] = useState<string>("")
 
-  const calculateYFromFreq = (freq: number) => {
-    const cents = intonation.intervalToCents(REFERENCE_FREQ, freq)
+
+
+  const calculateYFromCents = (cents: number) => {
     const yValue = ((cents - CENTS_MIN) / (CENTS_MAX - CENTS_MIN)) * (-1 * VBOX_H) + VBOX_H
     return Number.isNaN(yValue) ? -1 : yValue
   }
+  const calculateYFromFreq = (freq: number) => {
+    const cents = intonation.intervalToCents(REFERENCE_FREQ, freq)
+    return calculateYFromCents(cents)
+  }
   const pitchPointY = calculateYFromFreq(props.pitch.frequency)
+
+
 
   const getPointsFromYValues = () => {
     const points = yValues
@@ -38,6 +48,7 @@ export function PitchFrame(props: PitchProps) {
     return `${points.charAt(0) === 'M' ? '' : 'M '}${points}`
   }
   
+
   useEffect(() => {
     setScrollBarX(scrollBarX + READING_INCREMENT)
 
@@ -45,12 +56,33 @@ export function PitchFrame(props: PitchProps) {
     setYValues(yValues)
 
     setPath(getPointsFromYValues())
-    console.log(path)
   }, [props.pitch.frequency])
+
+
 
   return (
     <View style={styles.container}>
       <svg viewBox={`0 0 ${VBOX_W} ${VBOX_H}`} xmlns="http://www.w3.org/2000/svg">
+
+      
+        {
+          Array(Math.ceil((2400 - -2400) / 1200))
+            .fill(-2400)
+            .map((x, y) => x + y * 1200)
+            .map((cents, ix) => {
+              return (
+                <rect key={ix} x={0} y={calculateYFromCents(cents) - 0.1} width={VBOX_W} height={0.2}/>
+              )
+            })
+        }
+
+
+        <rect x={0} y={calculateYFromCents(2400) - 0.2} width={VBOX_W} height={0.4}/>
+        <rect x={0} y={calculateYFromCents(1200) - 0.2} width={VBOX_W} height={0.4}/>
+        <rect x={0} y={calculateYFromCents(0) - 0.2} width={VBOX_W} height={0.4}/>
+        <rect x={0} y={calculateYFromCents(-1200) - 0.2} width={VBOX_W} height={0.4}/>
+        <rect x={0} y={calculateYFromCents(-2400) - 0.2} width={VBOX_W} height={0.4}/>
+
         <path d={path} stroke="black" strokeWidth={0.5} fill="none"/>
         <rect x={scrollBarX} y={VBOX_H - 5} width={0.5} height={5}/>
         {
